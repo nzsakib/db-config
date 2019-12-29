ff9db3ef97398a38c594baff70dd8f6a03354d6f
Nazmus Sakib <sukku.mia@gmail.com>
2019-12-29 16:57:40 +0600

    Downgrade to laravel 5.6 and added npm assets

---
+++ src/Http/Controller/ConfigController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nzsakib\DbConfig\Http\Controller;
+
+use Illuminate\Routing\Controller;
+
+class ConfigController extends Controller
+{
+    public function index()
+    {
+        return 'hello world';
+    }
+}
